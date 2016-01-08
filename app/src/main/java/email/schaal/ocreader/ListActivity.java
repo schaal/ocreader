a7538dc419cca9a98989b0575949ba2c2c03a3ee
Daniel Schaal <daniel@schaal.email>
2016-01-08 12:21:20 +0100

    Add german translation, remove settings for now

--- app/src/main/java/email/schaal/ocreader/ListActivity.java
+++ app/src/main/java/email/schaal/ocreader/ListActivity.java
@@ -40,8 +40,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Base64InputStream;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.CompoundButton;
 
@@ -380,29 +378,6 @@ public class ListActivity extends RealmActivity implements ItemViewHolder.OnClic
                 SyncService.startSync(this);
             }
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_list, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        switch (id) {
-            case R.id.action_settings:
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
