77b9503e7241fc3f050a9f0c2d5ebd124c782936
Daniel Schaal <daniel@schaal.email>
2016-06-05 09:32:42 +0200

    Get material colors from Materialize

--- app/src/main/java/email/schaal/ocreader/util/ColorGenerator.java
+++ app/src/main/java/email/schaal/ocreader/util/ColorGenerator.java
@@ -24,6 +24,8 @@
 
 package email.schaal.ocreader.util;
 
+import com.mikepenz.materialize.color.Material;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -32,35 +34,24 @@ import java.util.List;
  * @datetime 14 Oct 2014, 5:20 PM
  */
 public class ColorGenerator {
-    public static ColorGenerator MATERIAL;
-
-    static {
-        MATERIAL = create(Arrays.asList(
-                0xffe57373,
-                0xfff06292,
-                0xffba68c8,
-                0xff9575cd,
-                0xff7986cb,
-                0xff64b5f6,
-                0xff4fc3f7,
-                0xff4dd0e1,
-                0xff4db6ac,
-                0xff81c784,
-                0xffaed581,
-                0xffff8a65,
-                0xffd4e157,
-                0xffffd54f,
-                0xffffb74d,
-                0xffa1887f,
-                0xff90a4ae
+    public final static ColorGenerator MATERIAL = new ColorGenerator(Arrays.asList(
+                Material.Red._500.getAsColor(),
+                Material.Purple._500.getAsColor(),
+                Material.Indigo._500.getAsColor(),
+                Material.Blue._500.getAsColor(),
+                Material.LightBlue._500.getAsColor(),
+                Material.Cyan._500.getAsColor(),
+                Material.Teal._500.getAsColor(),
+                Material.Green._500.getAsColor(),
+                Material.LightGreen._500.getAsColor(),
+                Material.Lime._500.getAsColor(),
+                Material.Amber._500.getAsColor(),
+                Material.DeepOrange._500.getAsColor(),
+                Material.Brown._500.getAsColor(),
+                Material.BlueGrey._500.getAsColor()
         ));
-    }
 
     private final List<Integer> mColors;
-
-    public static ColorGenerator create(List<Integer> colorList) {
-        return new ColorGenerator(colorList);
-    }
 
     private ColorGenerator(List<Integer> colorList) {
         mColors = colorList;
