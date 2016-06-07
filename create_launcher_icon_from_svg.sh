3f81e233f2b79ec9e2983cc31655f390264e6143
Daniel Schaal <daniel@schaal.email>
2016-06-07 18:02:34 +0200

    Add debug launcher icon with different color

--- create_launcher_icon_from_svg.sh
+++ create_launcher_icon_from_svg.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
 
 BASE_PATH=$(dirname "$0")
-MIPMAP_PATH="$BASE_PATH/app/src/main/res/mipmap"
-LOGO_SVG="$BASE_PATH/ic_launcher.svg"
+MIPMAP_PATH="$BASE_PATH/app/src/SECTION/res/mipmap"
+LOGO_SVG="$BASE_PATH/ic_launcher_SECTION.svg"
 
 convert_size() {
-    SIZE=$1
-    RES=$2
-    convert "$LOGO_SVG" -resize "${SIZE}x${SIZE}" "$MIPMAP_PATH-$RES/ic_launcher.png"
+    SECTION=$1
+    SIZE=$2
+    RES=$3
+    DIR="${MIPMAP_PATH/SECTION/$SECTION}-$RES"
+    if [[ ! -e "$DIR" ]]
+    then
+        echo mkdir -p "$DIR"
+    fi
+    echo convert "${LOGO_SVG/SECTION/$SECTION}" -resize "${SIZE}x${SIZE}" "${DIR}/ic_launcher.png"
 }
 
-convert_size 48  mdpi
-convert_size 72  hdpi
-convert_size 96  xhdpi
-convert_size 144 xxhdpi
-convert_size 192 xxxhdpi
+convert_to_mipmap() {
+    SECTION=$1
+    convert_size $SECTION 48  mdpi
+    convert_size $SECTION 72  hdpi
+    convert_size $SECTION 96  xhdpi
+    convert_size $SECTION 144 xxhdpi
+    convert_size $SECTION 192 xxxhdpi
+}
+
+convert_to_mipmap main
+convert_to_mipmap debug
