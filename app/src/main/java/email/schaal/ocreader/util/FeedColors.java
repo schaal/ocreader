4a78326e9390a5b32a0df67d1ea1763093be2e1e
Daniel Schaal <daniel@schaal.email>
2016-10-26 06:58:41 +0200

    Save Palette in FeedColors

--- app/src/main/java/email/schaal/ocreader/util/FeedColors.java
+++ app/src/main/java/email/schaal/ocreader/util/FeedColors.java
@@ -4,7 +4,10 @@ import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.graphics.Palette;
+import android.support.v7.graphics.Target;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,7 +15,7 @@ import java.util.Map;
  * Generate text and background color for Feeds
  */
 public class FeedColors {
-    private final Map<Type, Integer> colorMap = new HashMap<>(Type.values().length);
+    private final Palette palette;
 
     public enum Type {
         TEXT,
@@ -20,21 +23,39 @@ public class FeedColors {
     }
 
     FeedColors(@NonNull Palette palette) {
-        colorMap.put(Type.TEXT, loadColor(palette.getDominantSwatch()));
-        colorMap.put(Type.BACKGROUND, loadColor(palette.getMutedSwatch()));
+        this.palette = palette;
     }
 
     FeedColors(@Nullable @ColorInt Integer color) {
-        colorMap.put(Type.BACKGROUND, color);
-        colorMap.put(Type.TEXT, color);
+        final Palette.Swatch swatch;
+
+        if(color != null) {
+            swatch = new Palette.Swatch(color, 1);
+        } else {
+            swatch = null;
+        }
+
+        palette = new Palette.Builder(Collections.singletonList(swatch)).addTarget(Target.MUTED).generate();
     }
 
-    private Integer loadColor(Palette.Swatch swatch) {
-        return swatch != null ? swatch.getRgb() : null;
-    }
+    @ColorInt public int getColor(@NonNull Type type, @ColorInt int defaultColor) {
+        final Palette.Swatch swatch;
 
-    public @ColorInt int getColor(@NonNull Type type, @ColorInt int defaultColor) {
-        final Integer color = colorMap.get(type);
-        return color != null ? color : defaultColor;
+        switch (type) {
+            case TEXT:
+                swatch = palette.getDominantSwatch();
+                break;
+            case BACKGROUND:
+                swatch = palette.getMutedSwatch();
+                break;
+            default:
+                swatch = null;
+                break;
+        }
+
+        if(swatch != null)
+            return swatch.getRgb();
+        else
+            return defaultColor;
     }
 }
