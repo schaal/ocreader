5545d8b0a3bc8118a7a674315b2b3e174b913efd
Daniel Schaal <daniel@schaal.email>
2016-11-27 06:18:58 +0100

    Remove unused import

--- app/src/main/java/email/schaal/ocreader/ListActivity.java
+++ app/src/main/java/email/schaal/ocreader/ListActivity.java
@@ -29,7 +29,6 @@ import android.content.SharedPreferences;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
