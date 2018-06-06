91586fd170b4ba1c948aa68fd20e6b94c1cb0cd2
tobiasKaminsky <tobias@kaminsky.me>
2018-06-06 13:02:58 +0200

    get save scroll state back again

--- app/src/main/java/email/schaal/ocreader/view/ArticleWebView.java
+++ app/src/main/java/email/schaal/ocreader/view/ArticleWebView.java
@@ -1,8 +1,11 @@
 package email.schaal.ocreader.view;
 
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.os.Parcel;
+import android.os.Parcelable;
 import android.preference.PreferenceManager;
 import android.support.annotation.ColorInt;
 import android.support.annotation.Keep;
@@ -252,5 +255,64 @@ public class ArticleWebView extends NestedScrollWebView {
                 iframe.remove();
             }
         }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        
+        
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Parcelable state) {
+        SavedState savedState = (SavedState) state;
+        savedScrollPosition = savedState.scrollPosition;
+
+        super.onRestoreInstanceState(state);
+    }
+
+    @Override
+    protected Parcelable onSaveInstanceState() {
+        SavedState savedState = new SavedState(super.onSaveInstanceState());
+        savedState.scrollPosition = getScrollY();
+
+        return savedState;
+    }
+
+    private static class SavedState extends BaseSavedState {
+        private int scrollPosition;
+
+        private SavedState(Parcel source) {
+            super(source);
+            scrollPosition = source.readInt();
+        }
+
+        @TargetApi(24)
+        public SavedState(Parcel source, ClassLoader loader) {
+            super(source, loader);
+        }
+
+        public SavedState(Parcelable superState) {
+            super(superState);
+        }
+
+        @Override
+        public void writeToParcel(Parcel out, int flags) {
+            super.writeToParcel(out, flags);
+            out.writeInt(scrollPosition);
+        }
+
+        public static final Parcelable.Creator<SavedState> CREATOR = new Creator<SavedState>() {
+            @Override
+            public SavedState createFromParcel(Parcel source) {
+                return new SavedState(source);
+            }
+
+            @Override
+            public SavedState[] newArray(int size) {
+                return new SavedState[size];
+            }
+        };
     }
 }
