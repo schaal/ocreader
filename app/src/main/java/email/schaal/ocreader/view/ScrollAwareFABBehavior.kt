b18179bfb0e2a7cf303b77f5396c69ed1f741245
Daniel Schaal <daniel@schaal.email>
2020-01-06 09:01:14 +0100

    Convert ScrollAwareFABBehavior to kotlin

---
+++ app/src/main/java/email/schaal/ocreader/view/ScrollAwareFABBehavior.kt
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 Daniel Schaal <daniel@schaal.email>
+ *
+ * This file is part of OCReader.
+ *
+ * OCReader is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OCReader is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCReader.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package email.schaal.ocreader.view
+
+import android.view.View
+import androidx.annotation.Keep
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.view.ViewCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.floatingactionbutton.FloatingActionButton.OnVisibilityChangedListener
+
+@Keep
+class ScrollAwareFABBehavior : FloatingActionButton.Behavior() {
+    override fun onStartNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, directTargetChild: View, target: View, axes: Int, type: Int): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target, axes, type)
+    }
+
+    override fun onNestedScroll(coordinatorLayout: CoordinatorLayout, child: FloatingActionButton, target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int, consumed: IntArray) {
+        super.onNestedScroll(coordinatorLayout, child, target, dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, consumed)
+        if (dyConsumed > 0 && child.visibility == View.VISIBLE) { // User scrolled down and the FAB is currently visible -> hide the FAB
+            child.hide(object : OnVisibilityChangedListener() {
+                override fun onHidden(fab: FloatingActionButton) { // Workaround bug in android support library
+// see http://b.android.com/230298
+                    fab.visibility = View.INVISIBLE
+                }
+            })
+        } else if (dyConsumed < 0 && child.visibility != View.VISIBLE) { // User scrolled up and the FAB is currently not visible -> show the FAB
+            child.show()
+        }
+    }
+}
