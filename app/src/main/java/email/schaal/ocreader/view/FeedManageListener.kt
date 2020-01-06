7cbe0533a9d757de4dcc415e777dab7f7b8c777a
Daniel Schaal <daniel@schaal.email>
2020-01-06 08:31:20 +0100

    Convert FeedManageListener to kotlin

--- app/src/main/java/email/schaal/ocreader/view/FeedManageListener.kt
+++ app/src/main/java/email/schaal/ocreader/view/FeedManageListener.kt
@@ -6,8 +6,8 @@ import email.schaal.ocreader.database.model.Feed
  * Callbacks for feed management
  */
 interface FeedManageListener {
-    fun addNewFeed(url: String?, folderId: Long, finishAfterAdd: Boolean)
-    fun deleteFeed(feed: Feed?)
+    fun addNewFeed(url: String, folderId: Long, finishAfterAdd: Boolean)
+    fun deleteFeed(feed: Feed)
     fun changeFeed(feedId: Long, folderId: Long)
-    fun showFeedDialog(feed: Feed?)
+    fun showFeedDialog(feed: Feed)
 }
