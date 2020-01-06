2f7b710c5970a2271ac626a25639580882f8f88c
Daniel Schaal <daniel@schaal.email>
2020-01-06 09:45:10 +0100

    Convert service to kotlin

---
+++ app/src/main/java/email/schaal/ocreader/service/SyncType.kt
@@ -0,0 +1,18 @@
+package email.schaal.ocreader.service
+
+/**
+ * Created by daniel on 02.04.17.
+ */
+enum class SyncType(val action: String) {
+    FULL_SYNC("email.schaal.ocreader.action.FULL_SYNC"),
+    SYNC_CHANGES_ONLY("email.schaal.ocreader.action.SYNC_CHANGES_ONLY"),
+    LOAD_MORE("email.schaal.ocreader.action.LOAD_MORE");
+
+    companion object {
+        operator fun get(action: String): SyncType? {
+            for (syncType in values()) if (syncType.action == action) return syncType
+            return null
+        }
+    }
+
+}
