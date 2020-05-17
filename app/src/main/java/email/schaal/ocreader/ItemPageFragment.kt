846e310b7b0d605182fd3863459b60193b02d04b
Daniel Schaal <daniel@schaal.email>
2020-05-17 08:58:53 +0200

    Simplify ItemPageFragment

--- app/src/main/java/email/schaal/ocreader/ItemPageFragment.kt
+++ app/src/main/java/email/schaal/ocreader/ItemPageFragment.kt
@@ -26,55 +26,51 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import email.schaal.ocreader.database.model.Item
 import email.schaal.ocreader.databinding.FragmentItemPagerBinding
-import email.schaal.ocreader.view.ArticleWebView
 
 /**
  * Fragment to display a single feed item using a WebView.
  */
 class ItemPageFragment : Fragment() {
     private lateinit var binding: FragmentItemPagerBinding
-    private lateinit var webView: ArticleWebView
 
     override fun onStart() {
         super.onStart()
-        val item: Item? = arguments?.getParcelable(ARG_ITEM)
-        webView.setItem(item)
+        binding.webView.setItem(arguments?.getParcelable(ARG_ITEM))
     }
 
     override fun onPause() {
         super.onPause()
-        webView.onPause()
+        binding.webView.onPause()
     }
 
     override fun onResume() {
-        webView.onResume()
+        binding.webView.onResume()
         super.onResume()
     }
 
     override fun onDestroy() {
-        webView.destroy()
+        binding.webView.destroy()
         super.onDestroy()
     }
 
     override fun onDestroyView() {
-        webView.settings.javaScriptEnabled = false
+        binding.webView.settings.javaScriptEnabled = false
         super.onDestroyView()
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentItemPagerBinding.inflate(inflater, container, false)
-        webView = binding.webView
         return binding.root
     }
 
     companion object {
         private const val ARG_ITEM = "ARG_ITEM"
         fun newInstance(item: Item?): ItemPageFragment {
-            val bundle = Bundle()
-            bundle.putParcelable(ARG_ITEM, item)
-            val fragment = ItemPageFragment()
-            fragment.arguments = bundle
-            return fragment
+            return ItemPageFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_ITEM, item)
+                }
+            }
         }
     }
 }
