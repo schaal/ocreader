d302d8c5ec8ac8551fe544469dbdf0d9e1ebb7c1
Daniel Schaal <daniel@schaal.email>
2020-05-20 08:43:20 +0200

    Simplify AddNewFeedDialogFragment

--- app/src/main/java/email/schaal/ocreader/view/AddNewFeedDialogFragment.kt
+++ app/src/main/java/email/schaal/ocreader/view/AddNewFeedDialogFragment.kt
@@ -27,26 +27,27 @@ class AddNewFeedDialogFragment : BottomSheetDialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val activity = requireActivity() as ManageFeedsActivity
-        val builder = AlertDialog.Builder(activity)
-        val binding = FragmentAddNewFeedBinding.inflate(activity.layoutInflater)
+
         val arguments = requireArguments()
         val feedId = arguments.getLong(ARG_FEED_ID, -1)
         val newFeed = feedId < 0
-        builder.setTitle(if (newFeed) R.string.add_new_feed else R.string.edit_feed)
-        binding.feedUrl.isEnabled = newFeed
-        val finishAfterClose: Boolean
-        binding.folder.adapter = activity.folderSpinnerAdapter
-        binding.feedUrl.setText(arguments.getString(ARG_URL))
-        binding.folder.setSelection(activity.folderSpinnerAdapter.getPosition(arguments.getLong(ARG_FOLDER_ID, 0)))
-        finishAfterClose = arguments.getBoolean(ARG_FINISH_AFTER_CLOSE, false)
-        builder.setPositiveButton(if (newFeed) R.string.add else R.string.save) { _, _ ->
-            if (newFeed)
-                listener?.addNewFeed(binding.feedUrl.text.toString(), binding.folder.selectedItemId, finishAfterClose)
-            else
-                listener?.changeFeed(feedId, binding.folder.selectedItemId)
+
+        val binding = FragmentAddNewFeedBinding.inflate(activity.layoutInflater).apply {
+            feedUrl.isEnabled = newFeed
+            folder.adapter = activity.folderSpinnerAdapter
+            feedUrl.setText(arguments.getString(ARG_URL))
+            folder.setSelection(activity.folderSpinnerAdapter.getPosition(arguments.getLong(ARG_FOLDER_ID, 0)))
         }
-        builder.setView(binding.root)
-        return builder.create()
+
+        return AlertDialog.Builder(activity).setTitle(if (newFeed) R.string.add_new_feed else R.string.edit_feed)
+                .setPositiveButton(if (newFeed) R.string.add else R.string.save) { _, _ ->
+                    if (newFeed)
+                        listener?.addNewFeed(binding.feedUrl.text.toString(), binding.folder.selectedItemId, arguments.getBoolean(ARG_FINISH_AFTER_CLOSE, false))
+                    else
+                        listener?.changeFeed(feedId, binding.folder.selectedItemId)
+                }
+                .setView(binding.root)
+                .create()
     }
 
     companion object {
@@ -62,16 +63,17 @@ class AddNewFeedDialogFragment : BottomSheetDialogFragment() {
          */
         fun show(activity: FragmentActivity, feed: Feed?, finishAfterClose: Boolean) {
             val dialogFragment = AddNewFeedDialogFragment()
-            val bundle = Bundle()
-            bundle.putBoolean(ARG_FINISH_AFTER_CLOSE, finishAfterClose)
-            if (feed != null) {
-                bundle.putString(ARG_URL, feed.url)
-                if (feed.id >= 0) {
-                    bundle.putLong(ARG_FEED_ID, feed.id)
-                    bundle.putLong(ARG_FOLDER_ID, feed.folderId)
+
+            dialogFragment.arguments = Bundle().apply {
+                putBoolean(ARG_FINISH_AFTER_CLOSE, finishAfterClose)
+                feed?.let { feed ->
+                    putString(ARG_URL, feed.url)
+                    if (feed.id >= 0) {
+                        putLong(ARG_FEED_ID, feed.id)
+                        putLong(ARG_FOLDER_ID, feed.folderId)
+                    }
                 }
             }
-            dialogFragment.arguments = bundle
             dialogFragment.show(activity.supportFragmentManager, "newfeed")
         }
     }
